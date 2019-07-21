c558929a7725a29ff499a4172779ab21a133d287
FerSR <yugi-fer@hotmail.com>
2019-07-21 12:14:32 -0500

    letvar

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,11 @@
+// let nombre = 'Wolverine'
+// if(true){
+//     let nombre = 'Magneto'
+//     console.log(nombre);
+// }
+// console.log(nombre);
+
+for(let i = 0; i<=5;i++){
+    console.log(i);
+}
+//console.log(i); falla por que i al ser let muere en el scope
