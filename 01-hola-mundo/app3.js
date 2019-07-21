b2d3157aaa867d797caa35c35695f3895936f687
FerSR <yugi-fer@hotmail.com>
2019-07-21 11:38:15 -0500

    clase 2

---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,11 @@
+console.log('Inicio del programa');
+setTimeout(() => {
+    console.log('primer timeout');
+}, 3000);
+setTimeout(() => {
+    console.log('segundo timeout');
+}, 0);
+setTimeout(() => {
+    console.log('tercer timeout');
+}, 0);
+console.log("fin del programa");
