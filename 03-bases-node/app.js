451a37948faaf18adbf8b479e557299d5d272059
FerSR <yugi-fer@hotmail.com>
2019-07-22 14:05:52 -0500

    yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,3 +1,31 @@
+const argv = require("yargs")
+  .command("listar", "Imprime en consola la tabla de multiplicar", {
+    base: {
+      demand: true,
+      alias: "b"
+    },
+    limite: {
+      alias: "l",
+      default: 10
+    }
+  })
+  .command("crear", "Crea una tabla de multiplicar", {
+    base: {
+      demand: true,
+      alias: "b"
+    },
+    limite: {
+      alias: "l",
+      default: 10
+    }
+  })
+  .command("borrar", "borra una tabla de multiplicar", {
+    base: {
+      demand: true,
+      alias: "b"
+    }
+  })
+  .help().argv;
 const {
   crearArchivo,
   leerArchivo,
@@ -5,25 +33,45 @@ const {
   BorrarArchivo
 } = require("./multiplicar/multplicar");
 
-console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split("=")[1];
+// console.log(process.argv);
+// let parametro = argv[2];
+// let base = parametro.split("=")[1];
+let base = argv.base;
+let limite = argv.limite;
+let command = argv._[0];
+switch (command) {
+  case "listar":
+    leerArchivo(base, limite)
+      .then(data => {
+        console.group("Data");
+        for (element of data.data) console.log(element);
+        console.groupEnd();
+        if (data.msg) console.group("Warning");
+        console.log(data.msg);
+        console.groupEnd();
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
-// crearArchivo(base)
-//   .then(archivo => {
-//     console.log(`se ha creado el archivo ${archivo}`);
-//   })
-//   .catch(err => console.log(err));
+    break;
+  case "crear":
+    crearArchivo(base, limite)
+      .then(archivo => {
+        console.log(`se ha creado el archivo ${archivo}`);
+      })
+      .catch(err => console.log(err));
+    break;
+  case "borrar":
+    BorrarArchivo(base).then((msg)=>{
+        console.log(msg);
+    }).catch(err=>console.log(err))
+    break;
+  default:
+    console.log("comando no valido");
+    break;
+}
 
-// leerArchivo(base)
-//     .then((data)=>{
-//         console.log(data);
-//     })
-//     .catch((err)=>{console.log(err);})
 // appendArchivo("hola mundo", base).then(msg => {
 //   console.log(`se ha concatenado ${msg}`);
 // }).catch((err)=>console.log(err))
-BorrarArchivo(base).then((msg)=>{
-    console.log(msg);
-}).catch(err=>console.log(err))
--- 03-bases-node/multiplicar/multplicar.js
+++ 03-bases-node/multiplicar/multplicar.js
@@ -5,15 +5,15 @@ const fs = require("fs");
 //archivos propios
 //const fs = requiere('path/modulo')
 //module.exports.crearArchivo
-let crearArchivo = base => {
+let crearArchivo = (base, limite) => {
   return new Promise((resolve, reject) => {
     let data = "";
-    if(!Number(base)){
-        reject(`El valor ${base} no es un numero`)
-        return
+    if (!Number(base)) {
+      reject(`El valor ${base} no es un numero`);
+      return;
     }
     let nombre = `tabla-${base}.txt`;
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= limite; i++) {
       data += `${base} * ${i} = ${base * i}\n`;
     }
     fs.writeFile(`tablas/${nombre}`, data, err => {
@@ -24,36 +24,43 @@ let crearArchivo = base => {
     });
   });
 };
-let leerArchivo = base =>{
-    return new Promise((resolve,reject)=>{
-        let nombre = `tabla-${base}.txt`;
-        fs.readFile(`./tablas/${nombre}`,'utf8',(err,data)=>{
-            if(err) reject(err);
-            resolve(data.split('\n'));
-        })
-    })
-}
-let appendArchivo = (mensaje,base) =>{
-    return new Promise((resolve,reject)=>{
-        let nombre = `tabla-${base}.txt`;
-        fs.appendFile(`./tablas/${nombre}`,mensaje,(err)=>{
-            if(err) reject(err)
-            resolve(mensaje)
-        })
-    })
-}
-let BorrarArchivo = (base)=>{
-    return new Promise((resolve,reject)=>{
-        let nombre = `tabla-${base}.txt`;
-        fs.unlink(`./tablas/${nombre}`,(err)=>{
-            if(err) reject(err)
-            resolve(`se ha eliminado el archivo ${nombre}`)
-        })
-    })
-}
+let leerArchivo = (base, limite) => {
+  return new Promise((resolve, reject) => {
+    let nombre = `tabla-${base}.txt`;
+    fs.readFile(`./tablas/${nombre}`, "utf8", (err, data) => {
+      if (err) reject(err);
+      else {
+        data = data.split("\n").filter(elemento => elemento.length != "");
+        let msg = "";
+        if (limite > data.length) {
+          msg = "hay menos elementos de los que solicitó";
+        }
+        resolve({ data, msg });
+      }
+    });
+  });
+};
+let appendArchivo = (mensaje, base) => {
+  return new Promise((resolve, reject) => {
+    let nombre = `tabla-${base}.txt`;
+    fs.appendFile(`./tablas/${nombre}`, mensaje, err => {
+      if (err) reject(err);
+      resolve(mensaje);
+    });
+  });
+};
+let BorrarArchivo = base => {
+  return new Promise((resolve, reject) => {
+    let nombre = `tabla-${base}.txt`;
+    fs.unlink(`./tablas/${nombre}`, err => {
+      if (err) reject(err);
+      resolve(`se ha eliminado el archivo ${nombre}`);
+    });
+  });
+};
 module.exports = {
-    crearArchivo,
-    leerArchivo,
-    appendArchivo,
-    BorrarArchivo
-}
+  crearArchivo,
+  leerArchivo,
+  appendArchivo,
+  BorrarArchivo
+};
